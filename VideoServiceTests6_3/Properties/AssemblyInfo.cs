ec01e98929e109e8217ca5f016e8c55f328be382
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-20 15:59:31 +0700

    6_3 Modify 1

---
+++ VideoServiceTests6_3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VideoServiceTests6_3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VideoServiceTests6_3")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a5c696b-0954-4ed5-b328-a1388afc613a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VideoServiceTests6_3/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using Testing6_3.Mocking;
+
+namespace VideoServiceTests6_3
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
+        }
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+
+            _fileReader.Setup(fr => fr.Read("Video.txt")).Returns("");
+
+            var result = _videoservice.ReadVideoTitle();
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+        [Test]
+        public void GetUnprocessedVideosCsv_NoUnprocesedVideos_ReturnAnEmptyString()
+        {
+
+        }
+    }
+}
